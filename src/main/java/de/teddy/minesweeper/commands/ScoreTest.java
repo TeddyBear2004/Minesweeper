0d72773e2ac2e3d492b54a42e0533f5ed753b00b
TeddyBear2004 <thorgeengelmann@gmail.com>
2021-07-23 14:43:42 +0200

    updated test class

--- src/main/java/de/teddy/minesweeper/commands/ScoreTest.java
+++ src/main/java/de/teddy/minesweeper/commands/ScoreTest.java
@@ -1,21 +1,10 @@
 package de.teddy.minesweeper.commands;
 
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.ProtocolManager;
-import com.comphenix.protocol.events.PacketContainer;
-import com.comphenix.protocol.wrappers.BlockPosition;
-import com.comphenix.protocol.wrappers.EnumWrappers;
-import com.comphenix.protocol.wrappers.WrappedChatComponent;
 import de.teddy.minesweeper.util.PacketUtil;
-import org.bukkit.Location;
-import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
-import java.lang.reflect.InvocationTargetException;
 
 public class ScoreTest implements CommandExecutor {
     @Override
