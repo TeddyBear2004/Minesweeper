699e39a31cdaf61e74207de782574dc79fe8d2b0
TeddyBear2004 <thorgeengelmann@gmail.com>
2021-07-23 14:42:19 +0200

    updated test class

--- src/main/java/de/teddy/minesweeper/commands/ScoreTest.java
+++ src/main/java/de/teddy/minesweeper/commands/ScoreTest.java
@@ -1,19 +1,27 @@
 package de.teddy.minesweeper.commands;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.wrappers.BlockPosition;
 import com.comphenix.protocol.wrappers.EnumWrappers;
+import com.comphenix.protocol.wrappers.WrappedChatComponent;
 import de.teddy.minesweeper.util.PacketUtil;
+import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class ScoreTest implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
         if(sender instanceof Player){
-            PacketUtil.sendSoundEffect(((Player)sender), Sound.BLOCK_STONE_PLACE, EnumWrappers.SoundCategory.BLOCKS, new BlockPosition(((Player)sender).getLocation().toVector()), 0.5F, 0);
+    PacketUtil.sendParticleEffect(((Player)sender).getPlayer(), ((Player)sender).getLocation());
         }
 
         return true;
