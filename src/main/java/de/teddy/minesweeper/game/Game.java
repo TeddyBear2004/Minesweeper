47362e5d2390986b7f316bd0f77b6e1a14d3eb0d
TeddyBear2004 <thorgeengelmann@gmail.com>
2021-09-11 01:21:10 +0200

    changed from one tuple2 field to two separate fields

--- src/main/java/de/teddy/minesweeper/game/Game.java
+++ src/main/java/de/teddy/minesweeper/game/Game.java
@@ -2,32 +2,32 @@ package de.teddy.minesweeper.game;
 
 import de.teddy.minesweeper.Minesweeper;
 import de.teddy.minesweeper.util.IsBetween;
-import de.teddy.minesweeper.util.Tuple2;
-
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public enum Game {
-    MAP10X10(new Tuple2<>(new Location(Minesweeper.WORLD, 53, 17, -331), new Location(Minesweeper.WORLD, 58, 20, -318.5)),
+    MAP10X10(new Location(Minesweeper.WORLD, 53, 17, -331), new Location(Minesweeper.WORLD, 58, 20, -318.5),
             10, 10),
 
-    MAP18X18(new Tuple2<>(new Location(Minesweeper.WORLD, 49, 16, -198), new Location(Minesweeper.WORLD, 57, 20, -177.5)),
+    MAP18X18(new Location(Minesweeper.WORLD, 49, 16, -198), new Location(Minesweeper.WORLD, 57, 20, -177.5),
             18, 40),
 
-    MAP24X24(new Tuple2<>(new Location(Minesweeper.WORLD, 49, 14, -63), new Location(Minesweeper.WORLD, 61, 18, -37.5)),
+    MAP24X24(new Location(Minesweeper.WORLD, 49, 14, -63), new Location(Minesweeper.WORLD, 61, 18, -37.5),
             24, 99),
 
-    MAP_SPECIAL(new Tuple2<>(new Location(Minesweeper.WORLD, 49, 14, -63), new Location(Minesweeper.WORLD, 61, 18, -37.5)),
+    MAP_SPECIAL(new Location(Minesweeper.WORLD, 49, 14, -63), new Location(Minesweeper.WORLD, 61, 18, -37.5),
             24, 70);
 
     static{
         for(Game value : values()){
-            value.locations.getB().setYaw(-180);
-            value.locations.getB().setPitch(30);
+            value.spawnPosition.setYaw(-180);
+            value.spawnPosition.setPitch(30);
         }
     }
 
@@ -37,69 +37,72 @@ public enum Game {
 
     private static final Map<Player, Game> playerLocation = new HashMap<>();
 
-    private static void switchToMap(Player p, Game g) {
-    	Game.stopWatching(p);
-    	Board b = runningGames.get(p);
-    	if(b != null) {
-    		Game.finishGame(p);
-    	}
-    	playerLocation.put(p, g);
+    private static void switchToMap(Player p, Game g){
+        Game.stopWatching(p);
+        Board b = runningGames.get(p);
+        if(b != null){
+            Game.finishGame(p);
+        }
+        playerLocation.put(p, g);
         p.setAllowFlight(true);
         p.setFlying(true);
         p.teleport(g.getViewingSpawn());
     }
 
-    private static void startWatching(Player p, Board b) {
-    	Game cur = playerLocation.get(p);
-    	if(cur != b.map) {
-    		switchToMap(p, b.map);
-    	}
+    private static void startWatching(Player p, Board b){
+        Game cur = playerLocation.get(p);
+        if(cur != b.map){
+            switchToMap(p, b.map);
+        }
         b.draw(Collections.singletonList(p));
-    	gameWatched.put(p, b);
-    	b.viewers.add(p);
+        gameWatched.put(p, b);
+        b.viewers.add(p);
     }
 
-    private static void stopWatching(Player p) {
-    	Board b = gameWatched.remove(p);
-    	if(b != null) {
-    		b.viewers.remove(p);
-    	}
+    private static void stopWatching(Player p){
+        Board b = gameWatched.remove(p);
+        if(b != null){
+            b.viewers.remove(p);
+        }
     }
 
-    public static void stopGames(Player p) {
-    	Board b = runningGames.get(p);
-    	if(b != null) {
+    public static void stopGames(Player p){
+        Board b = runningGames.get(p);
+        if(b != null){
             b.drawBlancField();
-    		b.finish();
-    		b.viewers.forEach(gameWatched::remove);
-    		b.viewers.clear();
-    	} else {
-    		stopWatching(p);
-    	}
-    	runningGames.remove(p);
+            b.finish();
+            b.viewers.forEach(gameWatched::remove);
+            b.viewers.clear();
+        }else{
+            stopWatching(p);
+        }
+        runningGames.remove(p);
     }
 
-	public static Game getGame(Player player) {
-		return playerLocation.get(player);
-	}
+    public static Game getGame(Player player){
+        return playerLocation.get(player);
+    }
 
 
-    private final Tuple2<Location, Location> locations;
+    private final Location corner;
+    private final Location spawnPosition;
     private final int size;
     private final int bombCount;
-    Game(Tuple2<Location, Location> location, int size, int bombCount){
-        this.locations = location;
+
+    Game(Location corner, Location spawnPoint, int size, int bombCount){
+        this.corner = corner;
+        this.spawnPosition = spawnPoint;
         this.size = size;
         this.bombCount = bombCount;
     }
 
     public boolean isBlockOutsideGame(Block block){
-        return !IsBetween.isBetween2D(locations.getA(), size, size, block)
-                || !IsBetween.isBetween(locations.getA().getBlockY(), locations.getA().getBlockY() + 1, block.getY());
+        return !IsBetween.isBetween2D(corner, size, size, block)
+                || !IsBetween.isBetween(corner.getBlockY(), corner.getBlockY() + 1, block.getY());
     }
 
     public int getFieldHeight(){
-        return locations.getA().getBlockY();
+        return corner.getBlockY();
     }
 
     public static Board getBoard(Player Player){
@@ -111,62 +114,62 @@ public enum Game {
     }
 
     public Location getViewingSpawn(){
-        return locations.getB();
+        return spawnPosition;
     }
 
     public Board getRunningGame(){
         return runningGames.values().stream().filter(b -> b.map == this).findFirst().orElse(null);
     }
 
-    public void startGame(Player p) {
-    	startGame(p, true);
+    public void startGame(Player p){
+        startGame(p, true);
     }
 
-    public void startGame(Player p, boolean shouldTeleport) {
+    public void startGame(Player p, boolean shouldTeleport){
         /*synchronized (waiting) {
         	while(!waiting.isEmpty()) {
         		Player wat = waiting.get(0);
                 startWatching(wat, b);
             }
         }*/
-    	stopGames(p);
-        Board b = new Board(this, size, size, bombCount, locations.getA(), p);
-		b.drawBlancField(Collections.singletonList(p));
+        stopGames(p);
+        Board b = new Board(this, size, size, bombCount, corner, p);
+        b.drawBlancField(Collections.singletonList(p));
         startWatching(p, b);
         runningGames.put(p, b);
         Bukkit.getOnlinePlayers().forEach(onPlayer -> {
-        	if(gameWatched.get(onPlayer) == null && b.map == playerLocation.get(onPlayer)) {
-        		startWatching(onPlayer, b);
-        	}
+            if(gameWatched.get(onPlayer) == null && b.map == playerLocation.get(onPlayer)){
+                startWatching(onPlayer, b);
+            }
         });
 
         p.getInventory().clear();
         p.getInventory().setContents(Inventories.gameInventory);
         p.setAllowFlight(true);
-        if(shouldTeleport) {
-        	p.setFlying(true);
-        	p.teleport(this.getViewingSpawn());
+        if(shouldTeleport){
+            p.setFlying(true);
+            p.teleport(this.getViewingSpawn());
         }
     }
 
-    public static void finishGame(Player p, boolean quit) {
-    	Game.getGame(p).finish(p);
+    public static void finishGame(Player p, boolean quit){
+        Game.getGame(p).finish(p);
     }
 
-    public static void finishGame(Player p) {
-    	finishGame(p, false);
+    public static void finishGame(Player p){
+        finishGame(p, false);
     }
 
     private void finish(Player p){
-    	stopGames(p);
-    	Board b = getRunningGame();
-    	if(b != null) {
-	        Bukkit.getOnlinePlayers().forEach(onPlayer -> {
-	        	if(gameWatched.get(onPlayer) == null && b.map == playerLocation.get(onPlayer)) {
-	        		startWatching(onPlayer, b);
-	        	}
-	        });
-    	}
+        stopGames(p);
+        Board b = getRunningGame();
+        if(b != null){
+            Bukkit.getOnlinePlayers().forEach(onPlayer -> {
+                if(gameWatched.get(onPlayer) == null && b.map == playerLocation.get(onPlayer)){
+                    startWatching(onPlayer, b);
+                }
+            });
+        }
         /*Board board = runningGames.remove(p);
 
         if(board == null)
@@ -191,10 +194,10 @@ public enum Game {
     }
 
     public void startViewing(Player player, Board runningGame){
-    	if(runningGame == null) {
-        	Game.switchToMap(player, Game.MAP10X10);
-    	} else {
-    		Game.startWatching(player, runningGame);
-    	}
+        if(runningGame == null){
+            Game.switchToMap(player, Game.MAP10X10);
+        }else{
+            Game.startWatching(player, runningGame);
+        }
     }
 }
