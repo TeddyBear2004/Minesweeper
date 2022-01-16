eabc97b36f29f8d15ec7f315985ad1e53865a1f0
TeddyBear2004 <thorgeengelmann@gmail.com>
2022-01-16 01:41:33 +0100

    auto import minesweeper world

--- src/main/java/de/teddy/minesweeper/Minesweeper.java
+++ src/main/java/de/teddy/minesweeper/Minesweeper.java
@@ -17,7 +17,14 @@ import org.bukkit.World;
 import org.bukkit.WorldCreator;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
 import java.util.Objects;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 public final class Minesweeper extends JavaPlugin {
     public static World WORLD;
@@ -29,6 +36,7 @@ public final class Minesweeper extends JavaPlugin {
 
         Inventories.loadInventories();
 
+        extractWorld();
         WORLD = new WorldCreator("MineSweeper").createWorld();
         Objects.requireNonNull(this.getCommand("start")).setExecutor(new StartCommand());
         Objects.requireNonNull(this.getCommand("resetResourcePack")).setExecutor(new ResetResourcePack());
@@ -47,16 +55,48 @@ public final class Minesweeper extends JavaPlugin {
         protocolManager.addPacketListener(new LeftClickEvent());
         protocolManager.addPacketListener(new OnResourcePackStatus());
 
-        /*protocolManager.addPacketListener(new PacketAdapter(this, ListenerPriority.NORMAL, PacketType.Play.Server.CHAT) {
-            @Override
-            public void onPacketSending(PacketEvent event){
-                System.out.println(event);
-            }
+    }
 
-            @Override
-            public void onPacketReceiving(PacketEvent event){
-                System.out.println(event);
+    private void extractWorld(){
+        File minesweeper = new File("MineSweeper");
+        if(minesweeper.exists())
+            return;
+
+        saveResource("MineSweeper.zip", true);
+        File zippedFile = new File(getDataFolder(), "MineSweeper.zip");
+        try{
+            ZipFile zipFile = new ZipFile(zippedFile);
+            Enumeration<? extends ZipEntry> entries = zipFile.entries();
+            int i = 0;
+            int entriesCount = zipFile.size();
+            while(entries.hasMoreElements()){
+                ZipEntry entry = entries.nextElement();
+                i++;
+                getLogger().info("Extracting world file: " + entry.getName() + " (" + i + "/" + entriesCount + ")");
+                if(entry.isDirectory())
+                    new File(entry.getName()).mkdirs();
+                else{
+                    InputStream inputStream = zipFile.getInputStream(entry);
+                    File file = new File(entry.getName());
+                    if(!file.exists()){
+                        file.getParentFile().mkdirs();
+                        file.createNewFile();
+                    }
+                    FileOutputStream fileOutputStream = new FileOutputStream(file);
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    while((length = inputStream.read(buffer)) > 0)
+                        fileOutputStream.write(buffer, 0, length);
+                    fileOutputStream.close();
+                }
             }
-        });*/
+            zipFile.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        zippedFile.delete();
+
+        if(zippedFile.getParentFile().listFiles().length == 0)
+            zippedFile.getParentFile().delete();
     }
 }
