1fe9c8decee85b53149d8ac36158aca3504519b3
TeddyBear2004 <thorgeengelmann@gmail.com>
2022-12-19 21:23:22 +0100

    implemented location based fly
    Took 10 minutes

--- src/main/java/de/teddy/minesweeper/events/GenericEvents.java
+++ src/main/java/de/teddy/minesweeper/events/GenericEvents.java
@@ -6,20 +6,49 @@ import de.teddy.minesweeper.game.Game;
 import de.teddy.minesweeper.game.inventory.Inventories;
 import de.teddy.minesweeper.game.painter.ArmorStandPainter;
 import de.teddy.minesweeper.game.painter.BlockPainter;
+import de.teddy.minesweeper.game.temporary.Area;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerGameModeChangeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerResourcePackStatusEvent;
 
 public class GenericEvents implements Listener {
 
+    private static Area fromOutsideToInside(PlayerMoveEvent event) {
+        for (Area area : Minesweeper.getAreas())
+            if (!area.isInArea(event.getFrom()) && event.getTo() != null && area.isInArea(event.getTo()))
+                return area;
+
+        return null;
+    }
+
+    private static Area fromInsideToOutside(PlayerMoveEvent event) {
+        for (Area area : Minesweeper.getAreas())
+            if (event.getTo() != null && !area.isInArea(event.getTo()) && area.isInArea(event.getFrom()))
+                return area;
+
+        return null;
+    }
+
+    private static Area isInside(Location location) {
+        for (Area area : Minesweeper.getAreas())
+            if (area.isInArea(location))
+                return area;
+
+        return null;
+
+    }
+
     @EventHandler
     public void onPlayerJoinEvent(PlayerJoinEvent event) {
         event.getPlayer().getInventory().setContents(Inventories.VIEWER_INVENTORY);
-        event.getPlayer().setAllowFlight(true);
+        Area inside = isInside(event.getPlayer().getLocation());
+        if (inside == null || inside.isTemporaryFlightEnabled())
+            event.getPlayer().setAllowFlight(true);
         Minesweeper.getTexturePackHandler().apply(event.getPlayer());
     }
 
@@ -32,11 +61,6 @@ public class GenericEvents implements Listener {
             if (board != null)
                 board.breakGame();
         }
-    }
-
-    @EventHandler
-    public void onPlayerGameModeChangeEvent(PlayerGameModeChangeEvent event) {
-        event.getPlayer().setAllowFlight(true);
     }
 
 
@@ -61,6 +85,28 @@ public class GenericEvents implements Listener {
         if (!watching) {
             if (Minesweeper.getGames().size() != 0)
                 Minesweeper.getGames().get(0).startViewing(event.getPlayer(), null);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+
+        Area area = fromInsideToOutside(event);
+        if (area != null) {
+            if(area.isTemporaryFlightEnabled()){
+                player.setFlying(false);
+                player.setAllowFlight(false);
+            }
+            return;
+        }
+
+        area = fromOutsideToInside(event);
+        if(area != null){
+            if(area.isTemporaryFlightEnabled()){
+                player.setAllowFlight(true);
+                player.setFlying(true);
+            }
         }
     }
 }
