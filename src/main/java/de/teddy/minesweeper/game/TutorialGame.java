90be0852a9776eeaaa7e1c11134ec1a2ab82fdeb
TeddyBear2004 <thorgeengelmann@gmail.com>
2023-01-12 22:33:46 +0100

    removed ConnectionBuilder from TutorialGame
    Took 30 seconds

--- src/main/java/de/teddy/minesweeper/game/TutorialGame.java
+++ src/main/java/de/teddy/minesweeper/game/TutorialGame.java
@@ -1,7 +1,6 @@
 package de.teddy.minesweeper.game;
 
 import de.teddy.minesweeper.game.exceptions.BombExplodeException;
-import de.teddy.minesweeper.util.ConnectionBuilder;
 import de.teddy.minesweeper.util.Language;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -15,8 +14,8 @@ public class TutorialGame extends Game {
     private final Board.Field[][] board;
     private final Board.Field startField;
 
-    public TutorialGame(Plugin plugin, GameManager gameManager, List<Game> games, Language language, ConnectionBuilder connectionBuilder, Location corner, Location spawn, Board.Field[][] board, String name, Material material, int inventoryPosition, Board.Field startField) {
-        super(plugin, gameManager, games, language, connectionBuilder, corner, spawn, -1, -1, name, material, inventoryPosition);
+    public TutorialGame(Plugin plugin, GameManager gameManager, List<Game> games, Language language, Location corner, Location spawn, Board.Field[][] board, String name, Material material, int inventoryPosition, Board.Field startField) {
+        super(plugin, gameManager, games, language, null, corner, spawn, -1, -1, name, material, inventoryPosition);
         this.board = board;
         this.startField = startField;
     }
